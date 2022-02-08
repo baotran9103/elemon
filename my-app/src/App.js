efc62ce8869f892132c045406f604c79e5029eee
baotran9103 <69776991+baotran9103@users.noreply.github.com>
2022-02-08 16:20:01 -0500

    s

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+import Elemon from "./Elemon";
+function App() {
+  return (
+    <div className="App">
+      <Elemon />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/Data.js
@@ -0,0 +1,40 @@
+export const data = [
+  {
+    bpdyPart: [
+      { type: 1, quality: 5, ability: 1, val: 70 },
+      { type: 2, quality: 6, ability: 2, val: 100 },
+      { type: 3, quality: 3, ability: 3, val: 30 },
+      { type: 4, quality: 3, ability: 4, val: 30 },
+      { type: 5, quality: 3, ability: 5, val: 30 },
+      { type: 6, quality: 3, ability: 6, val: 30 },
+    ],
+    level: 8,
+    lockTime: 1000,
+    point: 910,
+    points: [1699, 1606, 1577, 1396, 1490, 1580, 3, 3, 0],
+    skills: [{ level: 1 }, { level: 2 }, , { level: 3 }],
+    star: 2,
+  },
+];
+
+export const info = [
+  {
+    tokenID: "123",
+    lastPrice: 456789,
+    ownderAddres: "asd",
+    baseCardID: 16,
+    purity: 0,
+    class: 17,
+    quality: 4,
+  },
+];
+
+export const DefaultTemplate = {
+  Rarity: [
+    { value: "A", output: 1, output: 0.66 },
+    { text: "B", value: 2, output: 1 },
+    { text: "S", value: 3, output: 1.66 },
+    { text: "SS", value: 4, output: 2.66 },
+    { text: "SSS", value: 5, output: 4 },
+  ],
+};
